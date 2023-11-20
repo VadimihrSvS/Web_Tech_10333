d3ecd69adf5dc938fcff9ee37e37543f766b984f
VadimihrSvS <vadimirsvs@gmail.com>
2023-11-20 15:41:42 +0300

    Lab_1_Sokolovsky_10333

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNet_Projects.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
