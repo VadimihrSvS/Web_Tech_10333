6ae2bec2e7ecc862b2c293687e5094f34b3d77c1
VadimihrSvS <vadimirsvs@gmail.com>
2023-11-20 23:56:57 +0300

    Lab_2_Sokolovsky_10333

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,37 @@
-﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AspNet_Projects.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace AspNet_Projects.Controllers;
-
-public class HomeController : Controller
+namespace AspNet_Projects.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
+        private List<ListDemo> _listDemo;
+
+        public HomeController()
+        {
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ListItemValue=1, ListItemText="Item 1"},
+                new ListDemo{ListItemValue=2, ListItemText="Item 2"},
+                new ListDemo{ListItemValue=3, ListItemText="Item 3"}
+            };
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Text"] = "Лабораторная работа №2";
+            ViewData["SelectList"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+            return View();
+        }
     }
 
-    public IActionResult Index()
+    class ListDemo
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public int ListItemValue { get; set; }
+        public string? ListItemText { get; set; }
     }
 }
