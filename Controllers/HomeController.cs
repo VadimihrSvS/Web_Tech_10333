7664c4ec173a9dc23584272392e05159e36c5c6c
VadimihrSvS <vadimirsvs@gmail.com>
2023-11-21 03:19:05 +0300

    Lab_3_Sokolovsky_10333

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace AspNet_Projects.Controllers
 
         public IActionResult Index()
         {
-            ViewData["Text"] = "Лабораторная работа №2";
+            ViewData["Text"] = "Лабораторная работа №3";
             ViewData["SelectList"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
